6d39f5d12a1519360bf19c081037baa29095e278
mpenna <tworker@onyx.ove.com>
2017-06-15 13:23:26 -0400

    Added modal for update user

--- ofsui/src/app/+user/+updateuser/updateuser.module.ts
+++ ofsui/src/app/+user/+updateuser/updateuser.module.ts
@@ -7,13 +7,17 @@ import {SmartadminModule} from "../../shared/smartadmin.module";
 import {SmartadminDatatableModule} from "../../shared/ui/datatable/smartadmin-datatable.module";
 import { SearchformComponent } from './searchform/searchform.component';
 import { UserresulttableComponent } from './userresulttable/userresulttable.component';
+import {SmartadminValidationModule} from "../../shared/forms/validation/smartadmin-validation.module";
+import {SmartadminInputModule} from "../../shared/forms/input/smartadmin-input.module";
 
 @NgModule({
   imports: [
     SmartadminModule,
     CommonModule,
     usertableRouting,
-    SmartadminDatatableModule
+    SmartadminDatatableModule,
+    SmartadminValidationModule,
+    SmartadminInputModule
   ],
   declarations: [UpdateUserComponent, SearchformComponent, UserresulttableComponent]
 })
--- ofsui/src/app/+user/+updateuser/userresulttable/userresulttable.component.ts
+++ ofsui/src/app/+user/+updateuser/userresulttable/userresulttable.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {UserSearchService} from "../searchform/searchform.service";
 import {HttpExceptionHandler} from "../../../core/api/httpexceptionhandler";
 import {UserAPIService} from "../../../core/api/userapi.service";
 import {Response} from "@angular/http";
 import {Observable} from "rxjs";
+import {ModalDirective} from "ngx-bootstrap";
 
 
 @Component({
   selector: 'app-userresulttable',
   templateUrl: './userresulttable.component.html'})
 export class UserresulttableComponent implements OnInit {
+
+  @ViewChild('lgModal') public lgModal:ModalDirective;
 
   constructor(private userSearchService: UserSearchService,private userApi: UserAPIService,
               private httpExceptionHandler: HttpExceptionHandler) { }
@@ -19,6 +22,18 @@ export class UserresulttableComponent implements OnInit {
   public numPages: number[];
   public maxPage: number;
   public selectedPage:number=1;
+  public updaateUserId: string;
+
+  public role;
+  public activeflag;
+  public passwordConfirm;
+  public password1;
+  public email;
+  public lastname;
+  public firstname;
+
+  isPasswordConfirmError: boolean = false;
+  passWordConfirmMessage: string = "";
 
   ngOnInit() {
     this.userSearchService.searchResultAnnounced$.subscribe(
@@ -33,8 +48,6 @@ export class UserresulttableComponent implements OnInit {
           else {
               this.numPages=this.setNumPages(Math.floor((results.count/results.limit)));
           }
-
-          console.log(this.maxPage);
         }
     );
   }
@@ -68,15 +81,56 @@ export class UserresulttableComponent implements OnInit {
       this.getNextPageData(this.selectedPage);
   }
 
+  public showUpdateUserModal(user:any) {
+      this.updaateUserId = user.id;
+      this.role=user.role;
+      this.activeflag=user.activeFlag;
+      this.email = user.emailAddress;
+      this.lastname = user.lastName;
+      this.firstname = user.firstName;
+      this.password1 = undefined;
+      this.passwordConfirm = undefined;
+      this.isPasswordConfirmError = false;
+      this.passWordConfirmMessage= "";
+      this.lgModal.show();
+  }
+
+  public updateUser() {
+      console.log("Update User")
+      if(this.validatePasswordUpdate()) {
+          console.log("Password Validations Passed");
+      }
+  }
+
+  private validatePasswordUpdate():boolean {
+      if(this.isFieldPresent(this.password1)) {
+          if(this.isFieldPresent(this.passwordConfirm)) {
+            if(this.passwordConfirm===this.password1) {
+                return true;
+            }
+            else {
+                this.isPasswordConfirmError = true;
+                this.passWordConfirmMessage = "The passwords do not match please try again.";
+                this.passwordConfirm = undefined;
+                return false;
+            }
+          }
+          else {
+              this.isPasswordConfirmError = true;
+              this.passWordConfirmMessage = "You must confirm the password."
+            return false;
+          }
+      }
+
+      return true;
+  }
+
   private getNextPageData(page:number) {
       this.userApi.search(this.userSearchService.getRequest(), this.userSearchService.getPageLimit(), (this.userSearchService.getPageLimit()*(page-1)))
           .map(this.extractData)
           .catch(this.handleError)
           .subscribe(
               results => {
-                  console.log("Results: " );
-                  console.log(results);
-                  console.log(this.count);
                   this.items = results.items;
                   this.count = results.count;
               },
@@ -92,5 +146,13 @@ export class UserresulttableComponent implements OnInit {
 
   private handleError(error:any) {
       return Observable.throw(error);
+  }
+
+  private isFieldPresent(field: any):boolean {
+    if(field == null || field == "") {
+        return false;
+    }
+
+    return true;
   }
 }
