24343422bd2a01b81de20da448dee0bade4c6559
mpenna <tworker@onyx.ove.com>
2017-07-21 16:30:24 -0400

    Added functionality to get all templates by company id

--- ofsui/src/app/+inventory/+templatemanager/viewandupdatetemplate/templatetable/templatetable.component.ts
+++ ofsui/src/app/+inventory/+templatemanager/viewandupdatetemplate/templatetable/templatetable.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {TemplateAPIService} from "../../../../core/api/templateapi.service";
+import {HttpExceptionHandler} from "../../../../core/api/httpexceptionhandler";
+import {Observable} from "rxjs";
+import {Response} from "@angular/http";
+
 
 @Component({
   selector: 'app-templatetable',
@@ -6,9 +11,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplatetableComponent implements OnInit {
 
-  constructor() { }
+  public items: any[];
+  public tableColumnNames: string[] = [];
+  public maxPropListSize =1;
+
+  constructor( private templateService: TemplateAPIService,
+               private httpExceptionHandler: HttpExceptionHandler) { }
 
   ngOnInit() {
+    this.templateService.getTemplateByCompanyId()
+        .map(this.extractData)
+        .catch(this.handleError)
+        .subscribe(
+            result => {
+              this.buildTableColumnNames(result.items)
+              this.buildItemList(result.items)
+            },
+            error => {
+              this.httpExceptionHandler.handleException(error)
+              console.log(error)
+            }
+        )
   }
 
+  private buildItemList(templateList: any[]){
+    var data = [];
+    var items = []
+    var itemLocation = 0;
+    for(let template of templateList) {
+      data = [];
+      data[0] = template.id
+      data[1] = template.name
+      console.log("Here")
+
+      for(var i = 0; i < this.maxPropListSize; i++) {
+        console.log("Looping Here: "+ i)
+        if(template.props.length > i) {
+          console.log("Not OB here at: "+ i)
+          data[data.length]= template.props[i].name
+          data[data.length]= template.props[i].type
+          data[data.length]= template.props[i].required
+        }
+        else {
+          console.log("OB here at: "+ i)
+          data[data.length]= "N/A"
+          data[data.length]= "N/A"
+          data[data.length]= "N/A"
+        }
+      }
+
+      console.log(data)
+      items[itemLocation] = data
+      itemLocation++
+    }
+
+    console.log(items)
+    this.items = items
+  }
+
+  private buildTableColumnNames(templateList: any[]) {
+    var columnNames= [];
+
+    columnNames[columnNames.length] = "Id"
+    columnNames[columnNames.length] = "Name"
+
+    for(let template of templateList) {
+      if(template.props.length > this.maxPropListSize) {
+        this.maxPropListSize = template.props.length
+      }
+    }
+
+    for(var i = 0; i < this.maxPropListSize; i++) {
+      columnNames[columnNames.length] = "Column" + (i+1) + ": Name"
+      columnNames[columnNames.length] = "Column" + (i+1) + ": Type"
+      columnNames[columnNames.length] = "Column" + (i+1) + ": Required"
+    }
+
+    console.log(columnNames)
+    this.tableColumnNames = columnNames;
+  }
+
+  private extractData(res:Response) {
+    return res.json();
+  }
+
+  private handleError(error:any) {
+    return Observable.throw(error);
+  }
 }
--- ofsui/src/app/core/api/templateapi.service.ts
+++ ofsui/src/app/core/api/templateapi.service.ts
@@ -22,4 +22,11 @@ export class TemplateAPIService{
         let options = new RequestOptions({"headers": headers});
         return this.http.post("http://localhost:8083/inventory/template", JSON.stringify(template), options);
     }
+
+    getTemplateByCompanyId():Observable<any> {
+        let headers = new Headers({ "Authorization": "Bearer "+ this.currentUser.token,
+            "Content-Type" : "application/json"});
+        let options = new RequestOptions({ "headers": headers });
+        return this.http.get("http://localhost:8083/inventory/template/company/id/"+ this.currentUser.companyid + "?limit=50000&start=0", options);
+    }
 }
