410073af73f543d8835b47de21aa98e198365c00
mpenna <tworker@onyx.ove.com>
2017-07-25 08:33:27 -0400

    Added create modal

--- ofsui/src/app/+inventory/+inventorymanager/inventorycreateform/inventorycreateform.component.ts
+++ ofsui/src/app/+inventory/+inventorymanager/inventorycreateform/inventorycreateform.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ModalDirective} from "ngx-bootstrap";
 
 @Component({
   selector: 'app-inventorycreateform',
@@ -6,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventorycreateformComponent implements OnInit {
 
+  @ViewChild('inventoryCreateModal') public inventoryCreateModal:ModalDirective;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public showCreateInventoryModal() {
+    console.log("Inside show create inventory");
+    this.inventoryCreateModal.show();
+  }
+
+  public createInventory() {
+    console.log("Inside create inventory");
   }
 
 }
